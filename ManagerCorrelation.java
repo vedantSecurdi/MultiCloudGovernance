a596dc88a0ec11af09c235b358bb7b0f77a5767b
vedantSecurdi <147059525+vedantSecurdi@users.noreply.github.com>
2023-12-13 16:56:37 +0530

    Create ManagerCorrelation.java

---
+++ ManagerCorrelation.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class YourClassName {
+    public static void main(String[] args) {
+      
+        Map<String, String> returnMap = new HashMap<>();
+
+        String managerID = link.getAttribute("managerId");
+
+        returnMap.put("identityAttributeName", "employee_id");
+        returnMap.put("identityAttributeValue", managerID);
+
+    }
+}
