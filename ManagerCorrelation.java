18d6c8f96154bd0577e8961bdbc8ea536966c0a2
vedantSecurdi <147059525+vedantSecurdi@users.noreply.github.com>
2024-01-18 14:52:04 +0530

    Update ManagerCorrelation.java

--- ManagerCorrelation.java
+++ ManagerCorrelation.java
@@ -1,13 +1,16 @@
 import java.util.HashMap;
 import java.util.Map;
+import Sailpoint.object.Context;
 
 public class YourClassName {
     public static void main(String[] args) {
       
         Map<String, String> returnMap = new HashMap<>();
 
-        String managerID = link.getAttribute("managerId");
+         
 
+        Sailpoint.object.Context context = new SailPoint.object.Context();
+        String managerID = context.getAttribute("managerId");
         returnMap.put("identityAttributeName", "employee_id");
         returnMap.put("identityAttributeValue", managerID);
 
